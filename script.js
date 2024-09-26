6555f425ef3a4807c431efb910a90a02b501ee76
unknown <khalidabdi3101@gmail.com>
2024-09-26 05:41:47 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Good Luck 💪🏾
+function mostExpensive(jewelry) {
+    let highestPrice = 0;
+    let mostExpensiveName = '';
+
+    for (const [name, price] of Object.entries(jewelry)) {
+        if (price > highestPrice) {
+            highestPrice = price;
+            mostExpensiveName = name;
+        }
+    }
+
+    return `The most expensive one is the ${mostExpensiveName}`;
+}
+
+// Example usage:
+console.log(mostExpensive({
+    "Diamond Earrings": 980,
+    "Gold Watch": 250,
+    "Pearl Necklace": 4650
+})); // "The most expensive one is the Pearl Necklace"
+
+console.log(mostExpensive({
+    "Silver Bracelet": 280,
+    "Gemstone Earrings": 180,
+    "Diamond Ring": 3500
+}));
